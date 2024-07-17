da42d495b03d629324f84c814c2777624e3f178f
smaghili <97967312+smaghili@users.noreply.github.com>
2024-07-17 13:50:56 +0330

    upload backup file sql  to server

--- sqlar.py
+++ sqlar.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart, Command
 from aiogram.enums import ParseMode
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+import aiofiles
+import os
 
 from tools.language.handler import MESSAGE
 from tools.db import db
@@ -50,6 +52,21 @@ async def handle_sql_query(message: types.Message, bot: Bot):
         await message.reply(f"<b>{await MESSAGE('ERROR')}</b>\n\n<pre>{str(e)}</pre>", parse_mode=ParseMode.HTML)
     finally:
         await db.disconnect()
+
+@dp.message(content_types=types.ContentType.DOCUMENT)
+async def handle_document(message: types.Message):
+    if message.chat.id != int(ADMIN_CHATID):
+        await message.reply(await MESSAGE('BLOCK'))
+        return
+
+    document = message.document
+    if document.mime_type == 'application/sql' or document.file_name.endswith('.sql'):
+        file_path = f"/var/lib/marzban/mysql/db-backup/{document.file_name}"
+        
+        await bot.download_file_by_id(document.file_id, file_path)
+        await message.reply(f"File '{document.file_name}' has been saved successfully.")
+    else:
+        await message.reply("Please upload a valid .sql file.")
 
 async def main():
     await dp.start_polling(bot)
